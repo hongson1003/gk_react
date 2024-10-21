efd763e04131612e974741d789f80c18279e9147
hongsonit10 <hongsonit10@gmail.com>
2024-10-21 10:54:54 +0700

    add vaidator

--- 21138281_NguyenHongSon/app/index.jsx
+++ 21138281_NguyenHongSon/app/index.jsx
@@ -1,22 +1,22 @@
-import { StyleSheet, Text, View } from "react-native"
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import validator from 'validator';
 
 const Home = () => {
-    return (
-        <View style={styles.container}>
-            <Text>
-                Welcome to the home page!
-            </Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the home page!</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Home;
